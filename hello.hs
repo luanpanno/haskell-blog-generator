320c7c240e065cdcc2bdeea5bb95cfb53586d95d
Luan Panno <luanpanno@gmail.com>
2024-12-04 00:52:32 -0300

    Type signatures + lambda functions

--- hello.hs
+++ hello.hs
@@ -1,13 +1,28 @@
 main = putStrLn myhtml
 
-myhtml = makeHtml "My page title" "Hello, world!"
+myhtml :: String
+myhtml = makeHtml "Hello title" (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
 
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+makeHtml :: String -> String -> String
+makeHtml = (\title -> \content -> html_ (head_ (title_ title) <> body_ content))
 
-html_ content = "<html>" <> content <> "</html>"
+html_ :: String -> String
+html_ = el "html"
 
-body_ content = "<body>" <> content <> "</body>"
+body_ :: String -> String
+body_ = el "body"
 
-head_ content = "<head>" <> content <> "</head>"
+head_ :: String -> String
+head_ = el "head"
 
-title_ content = "<title>" <> content <> "</title>"
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el = (\tag -> \content -> "<" <> tag <> ">" <> content <> "</" <> tag <> ">")
