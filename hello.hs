9f1076e1ad44332f304a35f66f7d35265c92ee57
Luan Panno <luanpanno@gmail.com>
2024-12-07 14:44:01 -0300

    Improve types

--- hello.hs
+++ hello.hs
@@ -1,28 +1,52 @@
-main = putStrLn myhtml
+main :: IO ()
+main = putStrLn (render myhtml)
 
-myhtml :: String
-myhtml = makeHtml "Hello title" (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+        (h1_ "Heading")
+        ( append_
+            (p_ "Paragraph #1")
+            (p_ "Paragraph #2")
+        )
+    )
 
-makeHtml :: String -> String -> String
-makeHtml = (\title -> \content -> html_ (head_ (title_ title) <> body_ content))
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el
+        "html"
+        ( el "head" (el "title" title)
+            <> el "body" (getStructureString content)
+        )
+    )
 
-html_ :: String -> String
-html_ = el "html"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-body_ :: String -> String
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el = (\tag -> \content -> "<" <> tag <> ">" <> content <> "</" <> tag <> ">")
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
+
+getStructureString :: Structure -> String
+getStructureString content =
+  case content of
+    Structure str -> str
+
+newtype Html = Html String
+
+newtype Structure = Structure String
+
+type Title = String
